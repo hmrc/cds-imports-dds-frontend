29cb4fd605d9c356850b5e33bd1274ad33c8210f
david.ellam <ellamdav@users.noreply.github.com>
2019-10-15 16:19:53 +0100

    replace mocking in EoriWhitelistSpec with fake Configuration instances

--- test/uk/gov/hmrc/cdsimportsddsfrontend/config/EoriWhitelistSpec.scala
+++ test/uk/gov/hmrc/cdsimportsddsfrontend/config/EoriWhitelistSpec.scala
@@ -16,37 +16,33 @@
 
 package uk.gov.hmrc.cdsimportsddsfrontend.config
 
-import org.mockito.Mockito._
-import org.scalatestplus.mockito.MockitoSugar
 import play.api.Configuration
 import uk.gov.hmrc.cdsimportsddsfrontend.test.CdsImportsSpec
 
-class EoriWhitelistSpec extends CdsImportsSpec with MockitoSugar {
-
-  private val mockConfiguration = mock[Configuration]
+class EoriWhitelistSpec extends CdsImportsSpec {
 
   "EoriWhitelist.allows" should {
     "return true if whitelist is non-empty and contains given EORI" in {
-      when(mockConfiguration.getOptional[Seq[String]]("whitelist.eori")).thenReturn(Some(Seq("ALICE", "GB12345678", "BOB")))
-      val whitelist = new EoriWhitelist(mockConfiguration)
+      val fakeConfiguration = Configuration("whitelist.eori" -> Seq("ALICE", "GB12345678", "BOB"))
+      val whitelist = new EoriWhitelist(fakeConfiguration)
       whitelist.allows("GB12345678") must be(true)
     }
 
     "return false if whitelist is non-empty and does not contain given EORI" in {
-      when(mockConfiguration.getOptional[Seq[String]]("whitelist.eori")).thenReturn(Some(Seq("RITA", "SUE", "BOB")))
-      val whitelist = new EoriWhitelist(mockConfiguration)
+      val fakeConfiguration = Configuration("whitelist.eori" -> Seq("RITA", "SUE", "BOB"))
+      val whitelist = new EoriWhitelist(fakeConfiguration)
       whitelist.allows("GB12345678") must be(false)
     }
 
     "return true if whitelist is empty" in {
-      when(mockConfiguration.getOptional[Seq[String]]("whitelist.eori")).thenReturn(Some(Seq.empty))
-      val whitelist = new EoriWhitelist(mockConfiguration)
+      val fakeConfiguration = Configuration("whitelist.eori" -> Seq.empty)
+      val whitelist = new EoriWhitelist(fakeConfiguration)
       whitelist.allows("E V E R Y O N E ! ! !") must be(true)
     }
 
     "return true if whitelist is not configured" in {
-      when(mockConfiguration.getOptional[Seq[String]]("whitelist.eori")).thenReturn(None)
-      val whitelist = new EoriWhitelist(mockConfiguration)
+      val fakeConfiguration = Configuration()
+      val whitelist = new EoriWhitelist(fakeConfiguration)
       whitelist.allows("E V E R Y O N E ! ! !") must be(true)
     }
   }
