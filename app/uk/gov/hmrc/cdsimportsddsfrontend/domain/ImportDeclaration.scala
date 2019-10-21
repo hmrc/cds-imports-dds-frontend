be0b57e3def394666162152eae0b12994cff4d03
david.ellam <ellamdav@users.noreply.github.com>
2019-10-21 12:10:48 +0100

    first slice of import declaration model

---
+++ app/uk/gov/hmrc/cdsimportsddsfrontend/domain/ImportDeclaration.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsimportsddsfrontend.domain
+
+case class ImportDeclaration(
+                            declarationType: String, // Declaration/TypeCode = "IM"
+                            additionalDeclarationType: String, // Declaration/TypeCode = "Z"
+                            goodsItemNumber: String, // Declaration/GoodsShipment/GovernmentAgencyGoodsItem/SequenceNumeric = "1" // only 1 item for now
+                            totalNumberOfItems: String, // Declaration/GoodsItemQuantity = "1" // only 1 item for now
+                            requestedProcedureCode: String, // Declaration/GoodsShipment/GovernmentAgencyGoodsItem/GovernmentProcedure/CurrentCode = "40"
+                            previousProcedureCode: String, // Declaration/GoodsShipment/GovernmentAgencyGoodsItem/GovernmentProcedure/PreviousCode = "00"
+                            additionalProcedureCode: String // "000 or C07"
+                            )
